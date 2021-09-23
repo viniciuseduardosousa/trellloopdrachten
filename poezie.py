9973e2818db143f1715e14fc7bdbacf6f0e670f9
Vinicius Sousa <viniciussousa@Viniciuss-MacBook-Air.local>
2021-09-23 23:33:46 +0200

    final poezie

---
+++ poezie.py
@@ -0,0 +1,7 @@
+vers1 ="ik heet vinicius en als ik kan vliegen zijn we allemaal duiven\n"
+vers2 ="ik geloof dat wij samen kunnen groeien zoals druiven\n"
+vers3 ="ik geloof in 1 god en in 1 liefde\n"
+vers4 ="maar ik geloof in 1 haat, haat op haat, haat op slecht\n"
+vers5 ="maar vooral haat op zelfhaat."
+
+print(vers1 + vers2 + vers3 + vers4 + vers5)
