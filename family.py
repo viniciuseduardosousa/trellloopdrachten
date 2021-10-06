2bcd9e9ac72ccc8192a8f3b645afcdce73ebe3e6
Vinicius Sousa <viniciussousa@Viniciuss-MacBook-Air.local>
2021-10-06 12:16:50 +0200

    family ties

---
+++ family.py
@@ -0,0 +1,6 @@
+naam = "vinicius"
+leeftijd = 17
+kmnaarschool = 5.1
+woonplaats = "Amsterdam"
+Hobbys = ["gamen", "uitgaan"]
+print(f"Hallo, ik heet {str(naam)}, ik ben {int(leeftijd)} jaar oud. De afstand van mijn huis naar school zijn {kmnaarschool} km. Ik woon in {str(woonplaats)}. Mijn hobby's zijn {Hobbys[0]} en {Hobbys[1]}." ) 
