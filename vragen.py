2f3a25836834e9cd6958e1d0c2f7e886e61e25df
Vinicius Sousa <viniciussousa@Viniciuss-MacBook-Air.local>
2021-10-06 11:54:28 +0200

    vragen opdracht

---
+++ vragen.py
@@ -0,0 +1,7 @@
+vraag1 = input("Hoe heet je? \n")
+print("oh dus je heet " + vraag1 +".")
+vraag2 = input("dus " + vraag1 + ", hoe oud ben je? ")
+print("waw dus je bent al " + vraag2 + "!")
+volgendjaar = int(vraag2) + 1
+vraag3 = input(f"dus wanner wordt je {volgendjaar}?")
+print(f"oh dus je wordt {volgendjaar} om {vraag3}?" )
