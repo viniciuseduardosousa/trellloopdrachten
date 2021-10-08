132636a6cc892ca2963b522aa321b55ea7fecdd5
Vinicius Sousa <viniciussousa@Viniciuss-MacBook-Air.local>
2021-10-08 10:02:36 +0200

    slice wise

---
+++ slicewise.py
@@ -0,0 +1,20 @@
+tekst = 'De Python lessen worden gegeven voor Erik, Erwin en ook door Hidde'
+print(tekst[37:41]) 
+print(tekst[43:48]) 
+print(tekst[61:66])
+print("\n")
+
+tekst2 =" SD vakken zijn bijvoorbeeld Python, UXD, Frontend development en Backend development. Wat leuk!"
+print(tekst2[29:35])
+print(tekst2[37:40])
+print(tekst2[42:62])
+print(tekst2[66:85])
+print("\n")
+
+tekst3 = "Wat is hier het laatste woord?"
+print(tekst3[24:29])
+print("\n")
+
+tekst4 = "Het www is ontwikkeld vanaf 1989 door Tim Berners-Lee"
+print(tekst4[4:8])
+print(tekst4[29:32])
