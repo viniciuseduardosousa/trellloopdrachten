26470882c86a00926ec86f971bc61b5caf9852f2
Vinicius Sousa <viniciussousa@Viniciuss-MacBook-Air.local>
2021-10-08 10:13:25 +0200

    In Variables we Trust

---
+++ inVariablesweTrust.py
@@ -0,0 +1,16 @@
+charachter = "a"
+
+Float1 = 6.9
+
+Boolean1 = True
+Boolean2 = Boolean1
+
+Integer1 = 3
+Integer2 = 2
+Integer3 = Integer1 - Integer2
+
+String1 ="blabla"
+String2 ="bla"
+String3 = String1 + String2
+
+print(charachter, Float1, Boolean2, Integer3, String3)
